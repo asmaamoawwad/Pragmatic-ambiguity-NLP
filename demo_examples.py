be94ed106c4c7e9a739acbaf1e4777ffdd7b1060
asmaamoawwad <asmaamoawad111@gmail.com>
2025-12-28 07:52:42 -0500

    Add example sentences with their truth values

---
+++ demo_examples.py
@@ -0,0 +1,5 @@
+EXAMPLES = [
+    {"text": "I love my job, it is very rewarding.", "truth": "Literal"},
+    {"text": "Oh great, I missed the bus again.", "truth": "Sarcastic"},
+    {"text": "The weather is just perfect for a picnic (in a storm).", "truth": "Sarcastic"}
+]
